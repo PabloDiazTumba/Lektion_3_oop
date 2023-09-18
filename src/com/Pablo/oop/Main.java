ebbc49a477e4e318e51b9916c17d6ed983081f94
PabloDiazTumba <pablodiaz147@gmail.com>
2023-09-18 11:58:21 +0200

    new printlin

--- src/com/Pablo/oop/Main.java
+++ src/com/Pablo/oop/Main.java
@@ -7,14 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Is this the first COMMIT?");
+
         //Instantiate Object
         Student benny = new Student("Benny", 15, true);
         Student frida = new Student( "Frida", 22, false);
 
-        System.out.println("Name:" + benny.name + " Age:" + benny.age);
-        if (benny.isTired = true) {
-            System.out.println("Dang im tired bruh!");
-        }
         System.out.println("Name:" + frida.name + " Age:" + frida.age);
         if (frida.isTired = false) {
             System.out.println("Dang im tired bruh!");
